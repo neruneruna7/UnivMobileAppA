e0415b63e6a7c9330585ac1c21de2141f18564bc
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-05 12:10:33 +0900

    update: 画像変更ボタン

--- app/src/main/java/jp/ac/meijou/android/s221205059/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205059/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import jp.ac.meijou.android.s221205059.databinding.ActivityMainBinding;
@@ -11,6 +12,18 @@ import jp.ac.meijou.android.s221205059.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    // javaのenum使いづらい？
+    enum TextContent {
+        Gorira(R.string.gorira),
+        Kani(R.string.kani);
+
+        private int id;
+        private TextContent(int id) {
+            this.id = id;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding.button.setOnClickListener(view -> {
             binding.text.setText(R.string.kani);
+        });
+
+        binding.button2.setOnClickListener(View -> {
             binding.imageView2.setImageResource(R.drawable.baseline_restaurant_menu_24);
         });
 
