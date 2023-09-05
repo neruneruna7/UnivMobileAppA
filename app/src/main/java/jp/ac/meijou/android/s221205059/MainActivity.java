d4c904c2b1f0fd17da4a4cc9e51084cdecc657ce
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-05 13:25:48 +0900

    update: 演習7

--- app/src/main/java/jp/ac/meijou/android/s221205059/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205059/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
 
@@ -30,12 +32,33 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int[] texts = {TextContent.Gorira.id, TextContent.Kani.id};
+
         binding.button.setOnClickListener(view -> {
-            binding.text.setText(R.string.kani);
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+
         });
 
         binding.button2.setOnClickListener(View -> {
             binding.imageView2.setImageResource(R.drawable.baseline_restaurant_menu_24);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+            }
         });
 
     }
