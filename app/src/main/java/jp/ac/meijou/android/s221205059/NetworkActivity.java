ca9d85c5fa2871fd7e83bece789fde2daad50dc9
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-09-07 11:49:51 +0900

    update: gistを読む

---
+++ app/src/main/java/jp/ac/meijou/android/s221205059/NetworkActivity.java
@@ -0,0 +1,62 @@
+package jp.ac.meijou.android.s221205059;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s221205059.databinding.ActivityNetworkBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkActivity extends AppCompatActivity {
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    private final Moshi moshi = new Moshi.Builder().build();
+
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+
+    private ActivityNetworkBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNetworkBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        var request = new Request.Builder()
+                .url("https://api.github.com/gists/c2a7c39532239ff261be")
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var gist = gistJsonAdapter.fromJson(response.body().source());
+
+                Optional.ofNullable(gist)
+                        .map(g -> g.files.get("OkHttp.txt"))
+                        .ifPresent(gistFile -> {
+                            runOnUiThread(() -> binding.gistText.setText(gistFile.content));
+//                            Log.d("json", gistFile);
+                        });
+            }
+        });
+    }
+}
